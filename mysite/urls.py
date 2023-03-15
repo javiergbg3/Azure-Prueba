b7c7bff8bf47360eb53a3bbeeae4c6e3335ca98f
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-03-15 11:27:53 +0100

    Prueba cache

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+
+
+def home(request):
+    return HttpResponse('Home Page')
 
 urlpatterns = [
-    path('',RedirectView.as_view(url='/polls/', permanent=True)),
+    path('', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
